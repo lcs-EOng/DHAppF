9fa6929b32f223764ca3e7344b7af89b755e748f
Eason Ong <eong@lcs.on.ca>
2024-06-14 18:56:06 -0400

    Initial Commit

---
+++ DHAppF/DHAppFApp.swift
@@ -0,0 +1,17 @@
+//
+//  DHAppFApp.swift
+//  DHAppF
+//
+//  Created by Ong Eason on 14/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct DHAppFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
